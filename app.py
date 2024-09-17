3beac358f20ebeb9da2283463dccc85999ff9add
Erik Zovdun <erik.zovdun@gmail.com>
2024-09-17 14:45:28 +0300

    added file app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+number1 = float(input("Input number 1: "))
+number2 = float(input("Input number 2: "))
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+operation = input("Choose an operation:\n"
+                  "1 - add\n"
+                  "2 - subtract\n")
+
+match operation:
+    case "1":
+        print("Result: " + add(number1, number2))
+    case "2":
+        print("Result: " + subtract(number1, number2))
+    case _:
+        print("Incorrect operation!")
